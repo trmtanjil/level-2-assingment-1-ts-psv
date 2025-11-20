9108afb87f381f714cdf2809ef00b49a53c5f9f3
trmtanjil <trmtanjil02@gmail.com>
2025-11-20 10:54:44 +0600

    // 2nd problem commit

--- src/assingment-1-provlemSolveTS.ts
+++ src/assingment-1-provlemSolveTS.ts
@@ -12,12 +12,18 @@ else{
     return !Value
 }
 }
-console.log(formatValue('talksjf'))
+// console.log(formatValue('talksjf'))
 
 
 
 // 2nd problem 
 
-function getLength (){
-    
+function getLength(value:string|any[] ) {
+  if (typeof value === 'string' || Array.isArray(value)) {
+    return value.length;
+  } else {
+    return 0;
+  }
 }
+console.log(getLength('t '))
+console.log(getLength([22,22,22,4]))
