d5aa27b8333b231649247c0093709d7ad24d6b9d
trmtanjil <trmtanjil02@gmail.com>
2025-11-20 11:41:06 +0600

    3nd problem commit commit

--- src/assingment-1-provlemSolveTS.ts
+++ src/assingment-1-provlemSolveTS.ts
@@ -25,5 +25,26 @@ function getLength(value:string|any[] ) {
     return 0;
   }
 }
-console.log(getLength('t '))
-console.log(getLength([22,22,22,4]))
+// console.log(getLength('t '))
+// console.log(getLength([22,22,22,4]))
+
+
+// problem 3 
+
+class Person{
+    name:string;
+    age:number;
+
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+    getDetails():string{
+        return `Name: ${this.name}, Age: ${this.age}`
+    }
+}
+const Person1 = new Person('tanjil',22)
+const Person2 = new Person('tanjila',19)
+
+console.log(Person1.getDetails())
+console.log(Person2.getDetails())
