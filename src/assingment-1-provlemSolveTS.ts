8d6e2b13d0ad6a08bdbfa69cd4d46145444909b4
trmtanjil <trmtanjil02@gmail.com>
2025-11-20 10:34:53 +0600

    first commit

---
+++ src/assingment-1-provlemSolveTS.ts
@@ -0,0 +1,23 @@
+
+//  1 problem solve 
+
+function formatValue (Value:number|string|boolean){
+if(typeof Value ==='number'){
+    return Value= Value*10
+}
+else if(typeof Value === 'string'){
+return Value.toUpperCase()
+}
+else{
+    return !Value
+}
+}
+console.log(formatValue('talksjf'))
+
+
+
+// 2nd problem 
+
+function getLength (){
+    
+}
